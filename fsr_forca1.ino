ca216fc5ce4f944a1d262d3c4352cf40946cb6a8
Sarah de Almeida Ramos <78384752+SarahdeAlmeidaRamos@users.noreply.github.com>
2023-09-13 16:25:04 -0300

    Add files via upload

---
+++ fsr_forca1.ino
@@ -0,0 +1,17 @@
+
+int Sensor = 0;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // sensor é igual a leitura do pino analogico1
+  Sensor = analogRead(A0);
+  // printa o valor no monitor serial
+  Serial.println(Sensor);
+  delay(10); // Delay para estabilizar
+}
